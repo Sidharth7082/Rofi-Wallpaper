be0f56dc7227e3d2513b6883acd4f411f42f07bd
Sidharth7082 <sidharthkaushal558@gmail.com>
2026-07-30 15:06:57 +0530

    code

--- rofi/scripts/wallpaper.sh
+++ rofi/scripts/wallpaper.sh
@@ -2,18 +2,11 @@
 
 WALL_DIR="$HOME/Pictures/Wallpapers"
 
-# Start swww daemon if not running
-if ! pgrep -x swww-daemon >/dev/null; then
-    swww-daemon &
-    sleep 1
-fi
-
 SELECTED=$(
 find "$WALL_DIR" -type f | while read -r file; do
     [[ "$file" =~ \.(jpg|jpeg|png|webp|gif|mp4|mkv|webm|JPG|JPEG|PNG|WEBP|GIF|MP4|MKV|WEBM)$ ]] || continue
 
     REL_PATH="${file#$WALL_DIR/}"
-
     printf "%s\0icon\x1f%s\n" "$REL_PATH" "$file"
 done | rofi \
     -dmenu \
@@ -31,20 +24,14 @@ EXT="${EXT,,}"
 
 case "$EXT" in
     gif|mp4|mkv|webm)
-        # Stop previous video wallpaper
         pkill -x mpvpaper 2>/dev/null
-
-        # Play animated wallpaper
         mpvpaper -o "--loop --mute" "*" "$FILE" &
         ;;
 
     *)
-        # Stop video wallpaper if running
         pkill -x mpvpaper 2>/dev/null
 
-        # Set image wallpaper
-        swww img "$FILE" \
-            --transition-type wipe \
-            --transition-duration 1
+        # Change image wallpaper
+        awww img "$FILE"
         ;;
 esac
